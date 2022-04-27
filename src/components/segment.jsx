a12c965715f613739eb564f7244b58c116304f9a
Avi Turkewitz <aviturkewitz@gmail.com>
2022-04-27 15:31:52 -0400

    simple css

--- src/components/segment.jsx
+++ src/components/segment.jsx
@@ -1,3 +1,5 @@
+import './segment.css'
+
 const Segment = ({index, segment, setSegment, removeSegment}) => {
   const {numberOfDaysDrinking, amountDrinkPerDay} = segment
 
@@ -8,25 +10,31 @@ const Segment = ({index, segment, setSegment, removeSegment}) => {
 
   return (
     <fieldset className='form-group'>
-      <button onClick={removeSegment}>−</button>
       <legend>Segment {index + 1}</legend>
-      <div>
-        <label className='input-label' htmlFor='number-of-days-drinking'>Drinking for how many days</label>
-        <input
-          type='number'
-          name='number-of-days-drinking'
-          value={numberOfDaysDrinking}
-          onChange={(e) => handleChange(e, 'numberOfDaysDrinking')}
-        />
-      </div>
-      <div>
-        <label className='input-label' htmlFor='amount-drink-per-day'>Amount drink per day (mL)</label>
-        <input
-          type='number'
-          name='amount-drink-per-day'
-          value={amountDrinkPerDay}
-          onChange={(e) => handleChange(e, 'amountDrinkPerDay')}
-        />
+      <div className='segment'>
+        <div> 
+          <div className='input-container'>
+            <label className='input-label' htmlFor='number-of-days-drinking'>Drinking for how many days</label>
+            <input
+              type='number'
+              name='number-of-days-drinking'
+              value={numberOfDaysDrinking}
+              onChange={(e) => handleChange(e, 'numberOfDaysDrinking')}
+            />
+          </div>
+          <div className='input-container'>
+            <label className='input-label' htmlFor='amount-drink-per-day'>Amount drink per day (mL)</label>
+            <input
+              type='number'
+              name='amount-drink-per-day'
+              value={amountDrinkPerDay}
+              onChange={(e) => handleChange(e, 'amountDrinkPerDay')}
+            />
+          </div>
+        </div>
+        {index > 0 &&
+          <button onClick={removeSegment}>−</button>
+        }
       </div>
     </fieldset>
   )
